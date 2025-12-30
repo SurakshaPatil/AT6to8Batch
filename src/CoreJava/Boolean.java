f53cfb8fd84b4d226216d47f5e5670096e9b14ec
SurakshaPatil <surakshapatil1998@gmail.com>
2025-12-30 15:33:32 +0530

    Core Java examples

---
+++ src/CoreJava/Boolean.java
@@ -0,0 +1,15 @@
+package CoreJave;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		boolean istrue=true;
+		
+		if (istrue==true) {
+			System.out.println("This statement is true");
+		} else {
+			System.out.println("This statement is false ");
+		}
+	}
+
+}
